a5d35e8866621bb2f674411d36405b56959edd4a
royalBoni <bonifaceemmanuel13@gmail.com>
2024-05-01 15:24:55 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  console.log("rer");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <p>A culinary canvas about to change the whole landscape of recipes</p>
+        <p className="text-red-700">Components under construction</p>dfddhfkj
+      </div>
+    </main>
+  );
+}

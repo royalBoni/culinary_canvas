1f0dc8395b752c66dd2c224ff9392931d80b10e6
samrawitfe <samrawitfentaye1996@gmail.com>
2024-05-12 19:35:43 +0200

    fix api access

--- src/app/api/user/list/route.ts
+++ src/app/api/user/list/route.ts
@@ -4,10 +4,20 @@ export const dynamic = "force-dynamic";
 export const GET = async () => {
   try {
     const allUsers = await readUsers();
-    return Response.json(allUsers);
+    let response = new Response(JSON.stringify(allUsers), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+    return response;
   } catch (error) {
-    console.log(error);
-
-    return Response.json({ error: "Something went wrong" }, { status: 500 });
+    return new Response(JSON.stringify({ error: "Something went wrong" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 };

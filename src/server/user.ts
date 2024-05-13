fdd41176d385145527ec977b778cd5dcee9c366c
samrawitfe <samrawitfentaye1996@gmail.com>
2024-05-13 17:31:36 +0200

    fix error

--- src/server/user.ts
+++ src/server/user.ts
@@ -6,7 +6,7 @@ import { revalidatePath } from "next/cache";
 
 export const readUsers = async () => {
   try {
-    revalidatePath("/api/users"); // Refresh cache
+    revalidatePath("/api/users/list"); // Refresh cache
     const data = await db.select().from(users);
     console.log("Data fetched:", data);
     return data || [];

f4ee906d073739ed658f2745c9648e188eb2b48d
samrawitfe <samrawitfentaye1996@gmail.com>
2024-05-15 02:29:58 +0200

    cors fix

--- src/app/api/middleware.js
+++ src/app/api/middleware.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req: Request) {
+export function middleware(req) {
   const response = NextResponse.next();
 
   response.headers.set("Access-Control-Allow-Origin", "*");

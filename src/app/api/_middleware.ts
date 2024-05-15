6a505acfb08d95ea2e5ce286767f0467ef2615cd
samrawitfe <samrawitfentaye1996@gmail.com>
2024-05-15 02:24:41 +0200

    cord config for dev

---
+++ src/app/api/_middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req: Request) {
+  const response = NextResponse.next();
+
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  response.headers.set("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+      },
+    });
+  }
+
+  return response;
+}
